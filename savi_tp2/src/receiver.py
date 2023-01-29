2e981be86f5e5dbf514872444d042cdbd6fa90c3
GRibeiro00 <gribeiro@ua.pt>
2023-01-29 18:08:22 +0000

    receiver a gerar duas nuvens de pontos

--- savi_tp2/src/receiver.py
+++ savi_tp2/src/receiver.py
@@ -32,26 +32,32 @@ pcd_ds = original_pcd.voxel_down_sample(voxel_size=0.01)
 #   Convert clouds to numpy arrays
 # ---------------------------------------------------
 ptc_pointsX, ptc_pointsY, ptc_pointsZ, ptc_colorsR, ptc_colorsG, ptc_colorsB = cloud2array(original_pcd)
+ptcDS_pointsX, ptcDS_pointsY, ptcDS_pointsZ, ptcDS_colorsR, ptcDS_colorsG, ptcDS_colorsB = cloud2array(pcd_ds)
 # ---------------------------------------------------
 #   Create messages to send
 # ---------------------------------------------------
 ptc_msg = cloudArray()
-# ptcDS_msg = cloudArray()
+ptcDS_msg = cloudArray()
 ptc_msg.pointsX = ptc_pointsX
 ptc_msg.pointsY = ptc_pointsY
 ptc_msg.pointsZ = ptc_pointsZ
 ptc_msg.colorsR = ptc_colorsR
 ptc_msg.colorsG = ptc_colorsG
 ptc_msg.colorsB = ptc_colorsB
-# ptcDS_msg.points = ptcDS_points
-# ptcDS_msg.colors = ptcDS_colors
+
+ptcDS_msg.pointsX = ptcDS_pointsX
+ptcDS_msg.pointsY = ptcDS_pointsY
+ptcDS_msg.pointsZ = ptcDS_pointsZ
+ptcDS_msg.colorsR = ptcDS_colorsR
+ptcDS_msg.colorsG = ptcDS_colorsG
+ptcDS_msg.colorsB = ptcDS_colorsB
 
 def talker():
     rospy.init_node('receiver', anonymous=False)
     rate = rospy.Rate(frame_rate) # 10hz
     while not rospy.is_shutdown():
         pub_ptc.publish(ptc_msg)
-        # pub_ptcDS.publish(ptcDS_msg)
+        pub_ptcDS.publish(ptcDS_msg)
         rate.sleep()
 
 if __name__ == '__main__':
